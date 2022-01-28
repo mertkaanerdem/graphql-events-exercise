c0a5d29c22f5043502ddb473bc2fcbbec3570e22
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-28 13:37:56 +0300

    types created

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
+const { ApolloServer, gql } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 const { events, users, locations, participants } = require("./data.json");
 
-console.log(participants);
+const typeDefs = gql`
+  type Event {
+    id: ID!
+    title: String!
+    desc: String!
+    date: String!
+    from: String!
+    to: String!
+    location_id: ID!
+    user_id: ID!
+  }
+
+  type Location {
+    id: ID!
+    name: String!
+    desc: String!
+    lat: Float!
+    lng: Float!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+  }
+
+  type Participant {
+    id: ID!
+    user_id: ID!
+    event_id: ID!
+  }
+
+  type Query {
+    #events
+    events: [Event!]!
+
+    #locations
+    locations: [Location!]!
+
+    #users
+    users: [User!]!
+
+    #participants
+    participants: [Participant!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    events: () => events,
+    locations: () => locations,
+    users: () => users,
+    participants: () => participants,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
