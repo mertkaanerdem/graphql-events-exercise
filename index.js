9b50fe5947283b080d8874703e50b7e4e9b840b0
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-28 14:19:13 +0300

    custom queries created

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const typeDefs = gql`
     to: String!
     location_id: ID!
     user_id: ID!
+    user: User!
+    location: Location!
+    participants: [Participant!]!
   }
 
   type Location {
@@ -28,6 +31,7 @@ const typeDefs = gql`
     id: ID!
     username: String!
     email: String!
+    events: [Event!]!
   }
 
   type Participant {
@@ -39,12 +43,15 @@ const typeDefs = gql`
   type Query {
     #events
     events: [Event!]!
+    event(id: ID!): Event!
 
     #locations
     locations: [Location!]!
+    location(id: ID!): Location!
 
     #users
     users: [User!]!
+    user(id: ID!): User!
 
     #participants
     participants: [Participant!]!
@@ -53,10 +60,38 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
+    // events & single event
     events: () => events,
+    event: (parent, args) => events.find((event) => event.id == args.id),
+
+    // locations
     locations: () => locations,
+    location: (parent, args) =>
+      locations.find((location) => location.id == args.id),
+
+    // users & single user
     users: () => users,
+    user: (parent, args) => {
+      const user = users.find((user) => user.id == args.id); //yukarıda query e eklenen user(id:ID!)... devamı ve &&tekil user'a ulaşmak için gerekli olan func.
+      if (!user) new Error("user not found");
+      return user;
+    },
+
+    // participants
     participants: () => participants,
+  },
+
+  //Custom Queries
+  Event: {
+    user: (parent, args) => users.find((user) => user.id === parent.user_id),
+    location: (parent, args) =>
+      locations.find((location) => location.id === parent.location_id),
+    participants: (parent, args) =>
+      participants.filter((participant) => participant.event_id === parent.id),
+  },
+  User: {
+    events: (parent, args) =>
+      events.filter((event) => event.user_id === parent.id),
   },
 };
 
